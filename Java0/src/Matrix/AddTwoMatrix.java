3ed71bfa41da809f8725b4157dfa34eb4b57bf8b
nazmul <nazmul.apu.hasan@gmail.com>
2016-04-28 21:28:32 +0300

    # add two matrix

---
+++ Java0/src/Matrix/AddTwoMatrix.java
@@ -0,0 +1,28 @@
+package Matrix;
+
+import java.util.Scanner;
+
+public class AddTwoMatrix {
+	
+	public static void main(String[] args) {
+		int m, n, c, d;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter the number of rows and columns of matrix");
+		m = in.nextInt();
+		n = in.nextInt();
+		
+		int first [][]= new int [m][n];
+		int second [][]= new int [m][n];
+		int sum [][]= new int [m][n];
+		
+		System.out.println("Enter the elements of first matrix");
+		
+		for (c = 0; c < m; c++) 
+			for (d =0; d<n; d++)
+				first[c][d] = in.nextInt();
+		
+		System.out.println("Enter the elements of second matrix");
+	}
+
+}
