2fb12f6dba46f809005da12e762c7ee8c7a89d43
nazmul <nazmul.apu.hasan@gmail.com>
2016-04-29 00:50:12 +0300

    #add matrix now

--- Java0/src/Matrix/AddTwoMatrix.java
+++ Java0/src/Matrix/AddTwoMatrix.java
@@ -23,6 +23,24 @@ public class AddTwoMatrix {
 				first[c][d] = in.nextInt();
 		
 		System.out.println("Enter the elements of second matrix");
+		
+		for (c = 0; c < m; c++ )
+			for (d = 0; d < n; d++)
+				second[c][d] = in.nextInt();
+		
+		for (c = 0; c < m; c++)
+			for (d = 0;d < n; d++  )
+				sum [c][d]= first[c][d] + second [c][d];
+		
+		System.out.println("Sum of entered matrices:-");
+		
+		for (c = 0; c < m; c++)
+		{
+			for (d =0; d < n; d++)
+				System.out.println(sum[c][d]+"\t");
+			
+			System.out.println();
+		}
 	}
 
 }
