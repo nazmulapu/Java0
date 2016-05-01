f82010abab0986e481f5882bcb46e838619b585e
nazmul <nazmul.apu.hasan@gmail.com>
2016-05-01 03:13:03 +0300

    # transpose matrix complete

--- Java0/src/Matrix/TransposeAMatrix.java
+++ Java0/src/Matrix/TransposeAMatrix.java
@@ -19,7 +19,24 @@ public class TransposeAMatrix {
 		for (c = 0; c <m; c++)
 			for (d = 0; d < n; d++)
 				matrix[c][d]= in.nextInt();
+		
 		int transpose [][] = new int [n][m];
+		
+		for (c = 0; c < m; c++)
+		{
+			for (d =0; d<n; d++)
+				transpose[d][c]= matrix[c][d];
+			
+		}
+		
+		System.out.println("Transpose of entered matrix");
+		
+		for (c = 0; c < n; c++)
+		{
+			for (d =0; d<m; d++)
+				System.out.println(transpose[c][d]+ "\t");
+			System.out.println("\n");
+		}
 	}
 
 }
